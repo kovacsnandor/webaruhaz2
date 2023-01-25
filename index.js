bb9185a20e3bf43d63b898958633069c270f182b
kovacsnandor <kovacsnandor@gmail.com>
2023-01-25 09:26:46 +0100

    statikus tartalmak kiszolgálása: public

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 let port = 3000;
 const fs = require("fs");
+const path = require("path")
 const uniqid = require("uniqid");
 
 const dataFile = "./data/products.json";
 
 //Middleware
 app.use(express.json());
+//statikus tartalmak kiszolgálása
+app.use('/public', express.static('public'));
+
+//get home
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, "./frontend/index.html"));
+})
+
 
 app.get("/products", function (req, res) {
   fs.readFile(dataFile, (error, data) => {
