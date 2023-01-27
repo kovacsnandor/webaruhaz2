fb6f18030482b476ba77d3b3a3e8732d2fd017b3
kovacsnandor <kovacsnandor@gmail.com>
2023-01-27 12:08:25 +0100

    javítás

--- public/my.js
+++ public/my.js
@@ -1,1 +1,31 @@
 console.log("frontend: heló");
+const url = "http://localhost:3000/products/"
+
+
+async function getProducts(){
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+}
+
+async function postProduct(){
+    console.log("post");
+    let body = {
+        name: "Á10",
+        quantity: 99,
+        price: 6500,
+        type: "fémáru"
+    }
+
+    body = JSON.stringify(body);
+    
+    let response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: body
+      })
+}
+
+getProducts();
